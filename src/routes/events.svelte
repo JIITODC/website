95da4f873d0189d275c714855d16c575f373eba2
Aayush Kumar Sahu <54525741+aayushmau5@users.noreply.github.com>
2020-09-18 21:10:39 +0530

    Add Demo Content to the Event and Team Page (#3)

--- src/routes/events.svelte
+++ src/routes/events.svelte
@@ -6,11 +6,37 @@
 
 
 <div class="main-title">EVENTS</div>
+<div class="event-container">
+    <div class="left">
+        <img src="Poster.png" alt="event poster">
+    </div>
+    <div class="right">
+        <h2 class="title">HACKTOBERFEST</h2>
+        <p class="about">WIN A TSHIRT AT HACKTOBERFEST BY CONTRIBUTING TO OPEN SOURCE PROJECTS.</p>
+        <p class="time">31 FEB 2023 12:30AM</p>
+        <p class="location">LT5</p>
+    </div>
+</div>
 
 
 <style>
     .main-title {
         font-size: 3rem;
         font-weight: bold;
+    }
+
+    .event-container {
+        display: flex;
+        margin-top: 2rem;
+        padding-bottom: 1rem;
+        border-bottom: 1px solid white;
+    }
+
+    .right {
+        padding: 0 0 0 2rem;
+    }
+
+    .title {
+        font-size: 2rem;
     }
 </style>
--- src/routes/team.svelte
+++ src/routes/team.svelte
@@ -6,10 +6,47 @@
 
 
 <div class="main-title">TEAMS</div>
+<h2 class="team-type">Core Members</h2>
+<div class="team-container">
+  <div class="team-member">
+    <div class="left">
+      <img src="Poster.png" alt="event poster">
+    </div>
+    <div class="right">
+      <h2 class="name">HACKTOBERFEST</h2>
+      <p class="about">WORLD’S GREATEST DETECTIVE. HE SOLVES CRIME AND HATES THE WORLD. GO TO HIS REPO TO SOLVE MURDER
+        MYSTERIES.</p>
+      <div class="socails">
+        <a href="#">GITHUB</a>
+        <a href="#">FACEBOOK</a>
+        <a href="#">TWITTER</a>
+      </div>
+    </div>
+  </div>
+</div>
 
 
 <style>
   .main-title {
+    font-size: 3rem;
+    font-weight: bold;
+  }
+
+  .team-container {
+    margin-top: 2rem;
+    padding-bottom: 1rem;
+    border-bottom: 1px solid white;
+  }
+
+  .team-member {
+    display: flex;
+  }
+
+  .right {
+    padding: 0 0 0 2rem;
+  }
+
+  .name {
     font-size: 3rem;
     font-weight: bold;
   }
