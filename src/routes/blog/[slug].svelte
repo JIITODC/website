9245250b862a2c09974c1711b43c03de5b93cd5d
Akshat Agarwal <humancalico@disroot.org>
2020-09-29 19:32:07 +0530

    some changes

--- src/routes/blog/[slug].svelte
+++ src/routes/blog/[slug].svelte
@@ -30,7 +30,7 @@
     header p {
         color: #aaa;
         text-transform: uppercase;
-        font-family: Raleway, sans-serif;
+        font-family: Merriweather, sans-serif;
         font-weight: 600;
     }
 
