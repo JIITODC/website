259dc7f5c7790f4e34e15edd814a52ef9f889032
Akshat Agarwal <humancalico@disroot.org>
2020-09-29 06:06:44 +0530

    add mvp for blog

---
+++ src/components/Bio.svelte
@@ -0,0 +1,32 @@
+<style>
+    div {
+        align-items: center;
+        display: flex;
+    }
+
+    img {
+        width: 100px;
+        height: 100px;
+        border-radius: 100px;
+        /* border: 3px solid #fd6378; */
+        display: block;
+        margin-right: 20px;
+    }
+
+    p {
+        font-size: 1.125rem;
+    }
+</style>
+
+<div>
+    <!-- TODO -->
+    <img src="profile-pic.png" alt="JODC Logo" />
+    <p>
+        You can follow us on
+        <a href="https://www.twitter.com/jiitodc" target="_blank">Twitter</a>,
+        see some of my work on
+        <a href="https://www.github.com/jiitodc" target="_blank">GitHub</a>, or
+        read more about me on
+        <a href="https://www.jodc.tech" target="_blank">our website</a>.
+    </p>
+</div>
