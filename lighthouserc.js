7d81e2dbfee57154c25dbd40ab3c1d041d671bc0
Leonardo Alvarenga <leo.alvarenga08@hotmail.com>
2020-10-17 20:13:24 -0300

    run prettier and lint

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,12 +1,11 @@
 module.exports = {
-    ci: {
-      collect: {
-        url: ['http://localhost:3000/'],
-        startServerCommand: 'node __sapper__/build',
-      },
-      upload: {
-        target: 'temporary-public-storage',
-      },
+  ci: {
+    collect: {
+      url: ['http://localhost:3000/'],
+      startServerCommand: 'node __sapper__/build',
     },
-  };
-  
+    upload: {
+      target: 'temporary-public-storage',
+    },
+  },
+};
